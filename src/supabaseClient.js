f404db7eb364511e72f10e5010d07f5ed99d1d2c
offerdear <214771149+offerdear@users.noreply.github.com>
2025-08-01 09:09:21 -0700

    Delete src directory

--- src/supabaseClient.js
+++
@@ -1,9 +0,0 @@
-// src/supabaseClient.js
-
-import { createClient } from '@supabase/supabase-js';
-
-// Replace with your actual Supabase project URL and anon key
-const supabaseUrl = 'https://uzpyjrilollsndgrhqow.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6cHlqcmlsb2xsc25kZ3JocW93Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM4NTA2MjUsImV4cCI6MjA2OTQyNjYyNX0.bc0khF7d8rQlQq9kl_1rSu6V0Lkz-abPLbOhNFNXAHw';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
